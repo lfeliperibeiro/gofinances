16735d0c62895aa1cf40f4ecce5874d0358140b1
Felipe Ribeiro <piporibeiro@gmail.com>
2021-07-15 22:22:00 -0300

    style: create Dashboard Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/53883371?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Felipe</UserName>
+            </User>
+          </UserInfo>
+          <Icon name={'power'} />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
