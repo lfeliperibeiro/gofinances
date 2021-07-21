73ddac630d304c13e075fe975dbc1ed68cef4db6
Felipe Ribeiro <piporibeiro@gmail.com>
2021-07-21 19:51:36 -0300

    feat: pass routes in app

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
 
@@ -9,7 +10,8 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 import AppLoading from 'expo-app-loading';
-import { Register } from './src/screens/Register';
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +25,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
