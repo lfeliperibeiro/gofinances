8bcdba6c2de63a2900b58bcfa486c9aedc7f725c
Felipe Ribeiro <piporibeiro@gmail.com>
2021-07-24 20:19:39 -0300

    feat: add Status bar

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import {
 import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,6 +29,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle={'light-content'} />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
