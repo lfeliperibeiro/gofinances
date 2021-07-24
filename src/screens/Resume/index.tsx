511677205f1157c344c8d18414c53758dada39f3
Felipe Ribeiro <piporibeiro@gmail.com>
2021-07-24 19:40:28 -0300

    feat: finish list and start Select Month

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,14 +1,138 @@
-import React from 'react';
-import { Container, Header, Title } from './styles';
+import React, { useEffect, useState } from 'react';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  ChartContainer,
+  MonthSelect,
+  MonthSelectButton,
+  MonthSelectIcon,
+  Month,
+} from './styles';
 import { HistoryCard } from '../../components/HistoryCard';
+import { AsyncStorage } from 'react-native';
+import { categories } from '../../utils/categories';
+import { VictoryPie } from 'victory-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import theme from '../../global/styles/theme';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+
+type TransactionData = {
+  type: 'positive' | 'negative';
+  name: string;
+  amount: string;
+  category: string;
+  date: string;
+};
+
+type CategoryData = {
+  key: string;
+  name: string;
+  total: number;
+  totalFormated: string;
+  percent: string;
+  color: string;
+};
 
 export function Resume() {
+  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>(
+    [],
+  );
+  async function LoadData() {
+    const dataKey = '@gofinances:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+    const responseFormatted = response ? JSON.parse(response) : [];
+
+    const expensives = responseFormatted.filter(
+      (expensive: TransactionData) => expensive.type === 'negative',
+    );
+
+    const expensiveTotal = expensives.reduce(
+      (acc: number, expensive: TransactionData) => {
+        return acc + Number(expensive.amount);
+      },
+      0,
+    );
+
+    const totalByCategory: CategoryData[] = [];
+
+    categories.forEach((category) => {
+      let categorySum = 0;
+      expensives.forEach((expensive: TransactionData) => {
+        if (expensive.category === category.key) {
+          categorySum += Number(expensive.amount);
+        }
+      });
+      if (categorySum > 0) {
+        const total = categorySum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        });
+
+        const percent = `${((categorySum / expensiveTotal) * 100).toFixed(0)}%`;
+
+        totalByCategory.push({
+          key: category.key,
+          name: category.name,
+          color: category.color,
+          total: categorySum,
+          totalFormated: total,
+          percent,
+        });
+      }
+    });
+    setTotalByCategories(totalByCategory);
+  }
+  useEffect(() => {
+    LoadData().then();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>Resumo por categoria</Title>
       </Header>
-      <HistoryCard title={'Compras'} amount={'R$ 150,00'} color={'red'} />
+      <Content
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingBottom: useBottomTabBarHeight(),
+        }}
+      >
+        <MonthSelect>
+          <MonthSelectButton>
+            <MonthSelectIcon name={'chevron-left'} />
+          </MonthSelectButton>
+          <Month>Maio</Month>
+          <MonthSelectButton>
+            <MonthSelectIcon name={'chevron-right'} />
+          </MonthSelectButton>
+        </MonthSelect>
+        <ChartContainer>
+          <VictoryPie
+            data={totalByCategories}
+            x={'percent'}
+            y={'total'}
+            colorScale={totalByCategories.map((categorie) => categorie.color)}
+            style={{
+              labels: {
+                fontSize: RFValue(18),
+                fontWeight: 'bold',
+                fill: theme.colors.shape,
+              },
+            }}
+            labelRadius={50}
+          />
+        </ChartContainer>
+        {totalByCategories.map((item) => (
+          <HistoryCard
+            key={item.key}
+            title={item.name}
+            amount={item.totalFormated}
+            color={item.color}
+          />
+        ))}
+      </Content>
     </Container>
   );
 }
