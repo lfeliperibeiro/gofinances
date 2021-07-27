0c7a88af09dfbbc4c0b92226adc63d3351188a46
Felipe Ribeiro <piporibeiro@gmail.com>
2021-07-26 22:28:43 -0300

    feat: add AuthProvider

--- App.tsx
+++ App.tsx
@@ -15,6 +15,8 @@ import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppRoutes } from './src/routes/app.routes';
 import { StatusBar } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,7 +32,9 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle={'light-content'} />
-        <AppRoutes />
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
