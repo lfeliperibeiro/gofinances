a88c7393d254d0c3bf8466555cbfe7c54a335ad9
Felipe Ribeiro <piporibeiro@gmail.com>
2021-07-26 22:28:12 -0300

    feat: pass context for SignIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -13,8 +13,11 @@ import GoogleIcon from '../../assets/google.svg';
 import Logo from '../../assets/logo.svg';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { SignInSocialButton } from '../../components/SignInSocialButton';
+import { useAuth } from '../../hooks/auth';
 
 export function SignIn() {
+  const { user } = useAuth();
+
   return (
     <Container>
       <Header>
