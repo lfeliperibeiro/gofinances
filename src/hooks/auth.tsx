c00ca94150760ee1b71a3ea8f72a1377acbaa91a
Felipe Ribeiro <piporibeiro@gmail.com>
2021-07-26 22:26:44 -0300

    feat: create hook context

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, ReactNode, useContext } from 'react';
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface IAuthContextData {
+  user: User;
+}
+const AuthContext = createContext({} as IAuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const user = {
+    id: '1',
+    name: 'Felipe',
+    email: 'email@email.com',
+  };
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  return useContext(AuthContext);
+}
+
+export { AuthProvider, useAuth };
