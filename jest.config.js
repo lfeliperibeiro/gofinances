973ce353b018957dbbe91ac994093afe1f7577af
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-30 19:00:53 -0300

    chore: create jest.config

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: 'jest-expo',
+  testPathIgnorePattern: ['/node_modules', '/android', '/ios', '/.idea'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+};
