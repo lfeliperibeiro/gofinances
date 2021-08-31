da78272298ce0d1b6aa84dcb77c9bf7087258942
Felipe Ribeiro <piporibeiro@gmail.com>
2021-08-31 19:36:32 -0300

    chore: add jest styled components to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'jest-expo',
   testPathIgnorePattern: ['/node_modules', '/android', '/ios', '/.idea'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    'jest-styled-components',
+  ],
 };
